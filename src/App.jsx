529eb5ab23e230b87fcd17295d15ad5a637b5449
Babalola-Yusuf <yusufbabalola01@gmail.com>
2024-05-01 14:10:47 +0100

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import backgroundImage from './assets/bg-img.jpg'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
