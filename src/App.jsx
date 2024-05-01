0f616cfdff703251a817f9ad5b5f306baf29f5da
Babalola-Yusuf <yusufbabalola01@gmail.com>
2024-05-01 14:51:26 +0100

    added some functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import backgroundImage from './assets/bg-img.jpg'
+import DigitalClock from './DigitalClock'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      
+      <DigitalClock />
     </div>
   )
 }
